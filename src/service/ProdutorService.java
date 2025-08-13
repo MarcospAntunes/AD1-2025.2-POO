04790e71967e3453c735f44c02ce3e2e65aa0055
MarcospAntunes <108834121+MarcospAntunes@users.noreply.github.com>
2025-08-13 08:40:13 -0300

    feat: implementado Service para Produtor

---
+++ src/service/ProdutorService.java
@@ -0,0 +1,21 @@
+package src.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import src.model.Produtor;
+
+public class ProdutorService {
+  private static int contador = 0;
+  private List<Produtor> produtores = new ArrayList<>();
+
+  public Produtor cadastrarProdutor(String nome, int idade, String nacionalidade) {
+    Produtor produtor = new Produtor(contador++, nome, idade, nacionalidade);
+    produtores.add(produtor);
+
+    return produtor;
+  }
+
+  public boolean removerProdutor(int ID) { return produtores.removeIf(produtor -> produtor.getID() == ID); }
+  public List<Produtor> listarProdutores() { return new ArrayList<>(produtores); }
+  
+}
